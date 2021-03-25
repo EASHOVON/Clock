5b6945675a17b554f564f92124aa2460aa2e0459
MD. ASRAFUZZAMAN SUVON <53660996+EASHOVON@users.noreply.github.com>
2021-03-25 10:35:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log('Hare Krishna')
+
+
+function toggle_button(){
+    document.querySelector('body').classList.toggle('light');
+    if(document.body.classList.contains('light')){
+        console.log('Hare Krishna');
+        console.log('light mode');
+        localStorage.setItem('light_mode', ('on') );
+    }else{
+        console.log('dark mode');
+        localStorage.setItem('light_mode', 'off');
+    }
+    
+    
+}
+
+if(localStorage.getItem('light_mode') == 'on'){
+    document.body.classList.toggle('light');
+}
+
+const deg=6;
+const hrs = document.querySelector('#hr');
+const mns = document.querySelector('#mn');
+const scs = document.querySelector('#sc');
+
+ setInterval(() => {
+
+let day = new Date()
+let hh = day.getHours()*30;
+let mm = day.getMinutes()*deg;
+let ss = day.getSeconds()*deg;
+
+hrs.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+mns.style.transform = `rotateZ(${mm}deg)`;
+scs.style.transform = `rotateZ(${ss}deg)`;
+
+ })
+
+
+//  var class_name = document.querySelector('.light');
+//  console.log(class_name);
+//  console.log(typeof(class_name));
